a925586c79cec586ca0a7e01e245a49dda7e8bd7
dong hoon kim <donghoonkim1226@gmail.com>
2016-01-16 10:50:38 -0500

    function has been tested, and confirmed that it is working.

--- sums.js
+++ sums.js
@@ -1,3 +1,20 @@
-var params = process.argv.slice(2);
+// Array [1, 7, 3, 4]
 
-for (var = i; i < params.length; i++);
+var params = process.argv.slice(2)
+  var sum;
+  var sumArray = [];
+for( var i = 0 ; i < params.length; i++){
+  console.log('I ' + i + " " + params[i])
+  sum = 0;
+  for(var j = 0; j < params.length; j++){
+    console.log('Ja ' + j)
+    if( j === i){
+    }
+    else{
+      sum += parseFloat(params[j]);
+      console.log(sum)
+    }
+  }
+  sumArray[i] = sum
+}
+console.log(sumArray)
